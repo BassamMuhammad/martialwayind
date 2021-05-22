c992c8a53f3f6ea2b022d668d2f14606dbbc9718
BassamMuhammad <72605456+BassamMuhammad@users.noreply.github.com>
2021-05-23 03:06:38 +0500

    updated checkout and readme

--- src/pages/checkout-success.tsx
+++ src/pages/checkout-success.tsx
@@ -1,17 +1,21 @@
-import { Box, Typography } from "@material-ui/core"
+import { Box, Button, Typography } from "@material-ui/core"
 import * as React from "react"
 import Layout from "../components/Layout"
 import { v4 } from "uuid"
 import { getCart, setCart } from "../utils"
 import Seo from "../components/seo"
+import { navigate } from "gatsby"
 
 const CheckoutSuccess = () => {
   React.useEffect(() => {
-    getCart().forEach(item => {
-      fetch(
-        `/.netlify/functions/editProduct?quantity=${item.quantity}&entryId=${item.id}`
-      ).then(() => setCart([]))
-    })
+    //prevent direct access
+    if (document.referrer.length !== 0) {
+      getCart().forEach(item => {
+        fetch(
+          `/.netlify/functions/editProduct?quantity=${item.quantity}&entryId=${item.id}`
+        ).then(() => setCart([]))
+      })
+    }
   }, [])
 
   return (
@@ -24,10 +28,19 @@ const CheckoutSuccess = () => {
         justifyContent="center"
         alignItems="center"
       >
-        <Typography>
-          Order placed successfully. <br /> Your order id is: {v4()} <br />{" "}
-          Please save it somewhere for reference
-        </Typography>
+        {document.referrer.length !== 0 ? (
+          <Typography>
+            Order placed successfully. <br /> Your order id is: {v4()} <br />
+            Please save it somewhere for reference
+          </Typography>
+        ) : (
+          <Typography>
+            Not allowed to access directly
+            <Button color="primary" onClick={() => navigate("/cart")}>
+              Go to cart
+            </Button>
+          </Typography>
+        )}
       </Box>
     </Layout>
   )
